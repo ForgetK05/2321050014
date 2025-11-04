9dff5194330af5f8e3d21092332f619245e26a50
ForgetK <kiendt18102005@gmail.com>
2025-11-04 16:14:02 +0700

    add

--- Bài15/script.js
+++ Bài15/script.js
@@ -1,39 +1,53 @@
 let danhSachPhim = [
     {
         id: 1,
-        tenPhim: "Mưa đỏ ",
+        tenPhim: "Người đẹp và quái vật",
         namPhatHanh: 2025,
         tuoi: 16,
         thoiLuong: 2,
-        quocGia: "Việt Nam",
-        poster: "url('./img/mua-do2-1122.jpeg')"
+        quocGia: "Mỹ",
+        poster: "img/phim/beautyandthebeaets.jpg"
     },
-
     {
         id: 2,
-        tenPhim: "Conan",
-        namPhatHanh: 2024,
-        tuoi: 13,
-        thoiLuong: 1.5,
-        quocGia: "Nhật Bản",
-        poster: "url('./img/poster/conan_27.jpg')"
-
-    }
+        tenPhim: "Bỗng dưng trúng số",
+        namPhatHanh: 2025,
+        tuoi: 16,
+        thoiLuong: 2,
+        quocGia: "Mỹ",
+        poster: "img/phim/bongdungtrungso.jpg"
+    },
+    {
+        id: 3  ,
+        tenPhim: "Cám",
+        namPhatHanh: 2025,
+        tuoi: 16,
+        thoiLuong: 2,
+        quocGia: "Mỹ",
+        poster: "img/phim/cam.jpg"
+    },
+    
 ]
 
 let phimHienTai = danhSachPhim[0];
+// document.write("<h2>" + phimHienTai.tenPhim + "</h2>");
+// document.write("<p>Năm phát hành: " + phimHienTai.namPhatHanh + "</p>");
+// document.write("<p>Độ tuổi: " + phimHienTai.tuoi + "+</p>");
+// document.write("<p>Thời lượng: " + phimHienTai.thoiLuong + " giờ</p>");
+// document.write("<p>Quốc gia: " + phimHienTai.poster + "</p>");
+// document.write("<hr>");
 
 let banner = document.getElementsByClassName('banner')[0];
-let id = document.getElementById('selectPhim').value;
-let name = document.getElementById('name').value;
-let namPhatHanh = document.getElementById('namPhatHanh').value;
-let age = document.getElementById('age').value;
+// let id = document.getElementById('selectPhim').value;
+// let name = document.getElementById('name').value;
+// let namPhatHanh = document.getElementById('namPhatHanh').value;
+// let age = document.getElementById('age').value;
+
 
 function chonPhim(idPhim){
     for (let i = 0; i < danhSachPhim.length; i++){
         if(danhSachPhim[i].id == idPhim){
-            banner.style.backgroundImage = danhSachPhim[i].poster;
-
+            banner.style.backgroundImage = "url('" + danhSachPhim[i].poster + "')";
         }
     }
 }
