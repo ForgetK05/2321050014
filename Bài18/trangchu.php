a1da3aba3374b8028c78c79fe9e2887930e483d3
ForgetK <kiendt18102005@gmail.com>
2025-12-01 15:07:12 +0700

    add

--- Bài18/trangchu.php
+++ Bài18/trangchu.php
@@ -3,24 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Document</title>
 </head>
 <body>
-    
     <?php
     session_start();
     if(!isset($_SESSION["username"])){
-        header("location: login.php");
+        header('location: login.php');
     }
-    
-    
-    ?>
-    <h1>Trang Chủ</h1>
-    <?php 
-    echo "Xin chào ".$_SESSION["username"]."";
-    
-    
     ?>
 
+    <h1>Trang chủ</h1>
+    <?php
+        echo "xin chào " . $_SESSION["username"];
+    ?>
 </body>
 </html>
---
+++ Bài19/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    session_start();
+    if (!isset($_SESSION["username"])) {
+        header("location: login.php");
+    }
+    ?>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php?page_layout=trangchu">Trang chủ</a></li>
+                <li><a href="index.php?page_layout=phim">Phim</a></li>
+                <li><a href="index.php?page_layout=theloai">Thể loại</a></li>
+                <li><a href="index.php?page_layout=quocgia">Quốc gia</a></li>
+                <li><a href="index.php?page_layout=nguoidung">Người dùng</a></li>
+            </ul>
+            <ul>
+                <li><?php echo "Xin chào ". $_SESSION["username"]; ?></li>
+                <li><a href="index.php?page_layout=dangxuat">Đăng xuất</a></li>
+            </ul>
+        </nav>
+
+        <?php 
+        if(isset($_GET['page_layout'])){
+        switch($_GET['page_layout']){
+            case 'trangchu':
+                include "trangchu.php";
+                break;
+            case 'phim':
+                include "phim.php";
+                break;
+            case 'theloai':
+                include "theloai.php";
+                break;
+            case 'quocgia':
+                include "quocgia.php";
+                break;
+            case 'nguoidung':
+                include "nguoidung.php";
+                break;
+            case 'dangxuat':         
+                break;
+        }
+        }
+        
+        
+        ?>
+    </header>
+</body>
+
+</html>
